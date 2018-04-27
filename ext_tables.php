a50f60de5f7b4adcd0d844f649faf21757098ed7
Alexander Opitz <opitz@pluspol.info>
2018-04-27 14:40:26 +0200

    [TASK] CleanUp

--- ext_tables.php
+++ ext_tables.php
@@ -1,5 +1,4 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+defined('TYPO3_MODE') || die('Access denied.');
+
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'static/Advanced_Tables_Content/', 'Advanced Tables Content');
